b93b11a706c4cbcc6f21008e20951f28dff8fc7e
djangostefany <djangostefany@gmail.com>
2024-12-04 19:09:21 +0000

    contrato verificado

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const alchemyUrl = process.env.ALCHEMY_URL;
+const prKey = process.env.PRIVATE_KEY;
+const ethScanApi = process.env.ETHERSCAN_API_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: alchemyUrl,
+      accounts: [prKey],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ethScanApi,
+    },
+  },  
 };
