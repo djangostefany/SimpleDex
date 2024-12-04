ed3b29a819dee0f7b8bc932a5771756f50e02665
djangostefany <djangostefany@gmail.com>
2024-12-04 15:31:50 +0000

    SimpleDex finalizado

---
+++ ignition/modules/Greeter.js
@@ -0,0 +1,8 @@
+const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+const GreeterModule = buildModule("GreeterModule", (deployer) => {
+    const greet = deployer.contract("Greeter",["Hola Mundo"]);
+    return {greet};
+});
+
+module.exports = GreeterModule;
